4c43e03b8d0d6a3cdbbce8d38364e540a7c35c6c
Michael Pingleton <mnpingleton@gmail.com>
2024-07-24 09:13:23 -0400

    Added Variable and VariableBundle classes.

---
+++ src/main/java/Variable.java
@@ -0,0 +1,14 @@
+/*
+ * Created 24-Jul-2024
+ */
+
+public class Variable {
+
+    public String name;
+    public float value;
+
+    public Variable() {
+        name = "";
+        value = 0.f;
+    }
+}
---
+++ src/main/java/VariableBundle.java
@@ -0,0 +1,38 @@
+/*
+ * Created 24-Jul-2024
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VariableBundle {
+
+    protected List<Variable> variables;
+
+    public VariableBundle() {
+        variables = new ArrayList<>();
+    }
+
+    public void setValue(String name, float value) {
+        for (int i = 0; i < variables.size(); i++) {
+            if (variables.get(i).name.equals(name)) {
+                variables.get(i).value = value;
+            }
+        }
+
+        Variable v = new Variable();
+        v.name = name;
+        v.value = value;
+        variables.add(v);
+    }
+
+    public float getValue(String name) {
+        for (int i = 0; i < variables.size(); i++) {
+            if (variables.get(i).name.equals(name)) {
+                return variables.get(i).value;
+            }
+        }
+
+        return Float.NaN;
+    }
+}
