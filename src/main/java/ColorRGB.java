0701ac7bdf7d7ed58e7b250bbeec27edafa28247
Michael Pingleton <mnpingleton@gmail.com>
2025-03-03 11:43:05 -0500

    Added ColorRGB class.

---
+++ src/main/java/ColorRGB.java
@@ -0,0 +1,21 @@
+public class ColorRGB {
+
+    private float r, g, b;
+
+    public ColorRGB(float red, float green, float blue) {
+        this.r = red;
+        this.g = green;
+        this.b = blue;
+    }
+
+    @Override
+    public String toString() {
+        String tmp = "ColorRGB\n";
+
+        tmp += "R: " + r + "\n";
+        tmp += "G: " + g + "\n";
+        tmp += "B: " + b + "\n";
+
+        return tmp;
+    }
+}
