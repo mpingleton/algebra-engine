e1d4cf1c6bed0a03b8f77f1cf3ea72e96933ce53
Michael Pingleton <mnpingleton@gmail.com>
2025-03-03 11:58:39 -0500

    Added prototype Graph2D class.

---
+++ src/main/java/Graph2D.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Graph2D {
+
+    private Area2D graphArea;
+    private ArrayList<GraphEquation2D> equations;
+    private ArrayList<Point2D> points;
+
+    public Graph2D() {
+        this.graphArea = new Area2D();
+        this.equations = new ArrayList<>();
+        this.points = new ArrayList<>();
+    }
+}
