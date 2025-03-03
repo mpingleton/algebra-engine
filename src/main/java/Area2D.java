fc431b52a42fd7bafbf7eebc7e7f182de59525d3
Michael Pingleton <mnpingleton@gmail.com>
2025-03-03 11:20:45 -0500

    Added 2D and 3D areas.

---
+++ src/main/java/Area2D.java
@@ -0,0 +1,22 @@
+public class Area2D {
+
+    public double xMin, xMax;
+    public double yMin, yMax;
+
+    public Area2D() {
+        this.xMin = 0.0;
+        this.xMax = 0.0;
+        this.yMin = 0.0;
+        this.yMax = 0.0;
+    }
+
+    @Override
+    public String toString() {
+        String tmp = "Area2D: \n";
+
+        tmp += "X: (" + xMin + "-" + xMax + ")\n";
+        tmp += "Y: (" + yMin + "-" + yMax + ")\n";
+
+        return tmp;
+    }
+}
---
+++ src/main/java/Area3D.java
@@ -0,0 +1,26 @@
+public class Area3D {
+
+    public double xMin, xMax;
+    public double yMin, yMax;
+    public double zMin, zMax;
+
+    public Area3D() {
+        this.xMin = 0.0;
+        this.xMax = 0.0;
+        this.yMin = 0.0;
+        this.yMax = 0.0;
+        this.zMin = 0.0;
+        this.zMax = 0.0;
+    }
+
+    @Override
+    public String toString() {
+        String tmp = "Area3D: \n";
+
+        tmp += "X: (" + xMin + "-" + xMax + ")\n";
+        tmp += "Y: (" + yMin + "-" + yMax + ")\n";
+        tmp += "Z: (" + zMin + "-" + zMax + ")\n";
+
+        return tmp;
+    }
+}
