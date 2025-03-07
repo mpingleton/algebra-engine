69e82b49def31e76d628b12b347b93f2384de53f
Michael Pingleton <mnpingleton@gmail.com>
2025-03-06 21:12:25 -0500

    Abs class and its unit test.

--- src/main/java/us/pinglesaur/algebraengine/functions/Abs.java
+++ src/main/java/us/pinglesaur/algebraengine/functions/Abs.java
@@ -1,5 +1,7 @@
 package us.pinglesaur.algebraengine.functions;
 
+import us.pinglesaur.algebraengine.equations.Value;
+
 public class Abs extends Function {
 
     public Abs() {
@@ -16,7 +18,51 @@ public class Abs extends Function {
     }
 
     @Override
-    public double execute(double x) {
-        return Math.abs(x);
+    public Value[] forwardExecute(Value[] input, double rangeMin, double rangeMax) {
+        if (input.length != 1)
+            return null;
+        else if (input[0].type != Value.TYPE_CONSTANT)
+            return null;
+
+        Value[] v = new Value[1];
+        v[0] = new Value();
+        v[0].type = Value.TYPE_CONSTANT;
+        v[0].isImaginary = input[0].isImaginary;
+        v[0].isNegative = false;
+        v[0].isCoeffInit = true;
+        v[0].coeff = Math.abs(input[0].getConstant());
+
+        return v;
+    }
+
+    @Override
+    public Value[] reverseExecute(Value[] input, double rangeMin, double rangeMax) {
+        if (input.length != 1)
+            return null;
+        else if (input[0].type != Value.TYPE_CONSTANT)
+            return null;
+
+        double c = input[0].getConstant();
+        if (c > 0) {
+            Value[] v = new Value[2];
+
+            v[0] = new Value();
+            v[0].type = Value.TYPE_CONSTANT;
+            v[0].isNegative = false;
+            v[0].isImaginary = input[0].isImaginary;
+            v[0].isCoeffInit = true;
+            v[0].coeff = c;
+
+            v[1] = new Value();
+            v[1].type = Value.TYPE_CONSTANT;
+            v[1].isNegative = true;
+            v[1].isImaginary = input[0].isImaginary;
+            v[1].isCoeffInit = true;
+            v[1].coeff = c;
+
+            return v;
+        }
+
+        return new Value[0];
     }
 }
---
+++ src/test/java/us/pinglesaur/algebraengine/functions/AbsTest.java
@@ -0,0 +1,55 @@
+package us.pinglesaur.algebraengine.functions;
+
+import org.junit.jupiter.api.Test;
+import us.pinglesaur.algebraengine.equations.Value;
+import us.pinglesaur.algebraengine.equations.VariableBundle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AbsTest {
+
+    @Test
+    void testName() {
+        Abs f1 = new Abs();
+        assert(f1.name().equals("abs"));
+    }
+
+    @Test
+    void testForwardExecute() {
+        VariableBundle bundle = new VariableBundle();
+        Value[] v1 = new Value[1];
+        v1[0] = new Value(3.14159);
+        Value[] v2 = new Value[1];
+        v2[0] = new Value(-3.14159);
+
+        Abs f1 = new Abs();
+        Value[] o1 = f1.forwardExecute(v1, -100, 100);
+        Value[] o2 = f1.forwardExecute(v2, -100, 100);
+
+        assert(o1 != null && o2 != null);
+        assert(o1.length == 1);
+        assert(o2.length == 1);
+        assertEquals(3.14159, o1[0].getConstant());
+        assertEquals(3.14159, o2[0].getConstant());
+    }
+
+    @Test
+    void testReverseExecute() {
+        VariableBundle bundle = new VariableBundle();
+        Value[] v1 = new Value[1];
+        v1[0] = new Value(3.14159);
+        Value[] v2 = new Value[1];
+        v2[0] = new Value(-3.14159);
+
+        Abs f1 = new Abs();
+        Value[] o1 = f1.reverseExecute(v1, -100, 100);
+        Value[] o2 = f1.reverseExecute(v2, -100, 100);
+
+        assert(o1 != null && o2 != null);
+        assert(o1.length == 2);
+        assert(o2.length == 0);
+
+        assertEquals(3.14159, o1[0].getConstant());
+        assertEquals(-3.14159, o1[1].getConstant());
+    }
+}
