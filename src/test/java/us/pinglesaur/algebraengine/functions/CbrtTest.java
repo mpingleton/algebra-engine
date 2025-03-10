a387a3c5ea998a99128dc69e3b13be61aae1d25d
Michael Pingleton <mnpingleton@gmail.com>
2025-03-10 12:50:35 -0400

    Testing zero for cbrt and sqrt functions.

--- src/test/java/us/pinglesaur/algebraengine/functions/CbrtTest.java
+++ src/test/java/us/pinglesaur/algebraengine/functions/CbrtTest.java
@@ -21,18 +21,24 @@ public class CbrtTest {
         v1[0] = new Value(27);
         Value[] v2 = new Value[1];
         v2[0] = new Value(-27);
+        Value[] v3 = new Value[1];
+        v3[0] = new Value(0);
 
         Cbrt f1 = new Cbrt();
         Value[] o1 = f1.forwardExecute(v1, -100, 100);
         Value[] o2 = f1.forwardExecute(v2, -100, 100);
+        Value[] o3 = f1.forwardExecute(v3, -100, 100);
 
-        assert(o1 != null && o2 != null);
+        assert(o1 != null && o2 != null && o3 != null);
         assert(o1.length == 1);
         assert(o2.length == 1);
+        assert(o3.length == 1);
         assert(!o1[0].isImaginary);
         assert(o2[0].isImaginary);
+        assert(!o3[0].isImaginary);
         assertEquals(3, o1[0].getConstant());
         assertEquals(3, o2[0].getConstant());
+        assertEquals(0, o3[0].getConstant());
     }
 
     @Test
@@ -44,16 +50,21 @@ public class CbrtTest {
         Value[] v2 = new Value[1];
         v2[0] = new Value(3);
         v2[0].isImaginary = true;
+        Value[] v3 = new Value[1];
+        v3[0] = new Value(0);
+        v3[0].isImaginary = false;
 
         Cbrt f1 = new Cbrt();
         Value[] o1 = f1.reverseExecute(v1, -100, 100);
         Value[] o2 = f1.reverseExecute(v2, -100, 100);
+        Value[] o3 = f1.reverseExecute(v3, -100, 100);
 
-        assert(o1 != null && o2 != null);
+        assert(o1 != null && o2 != null && o3 != null);
         assert(o1.length == 1);
         assert(o2.length == 1);
-
+        assert(o3.length == 1);
         assertEquals(27, o1[0].getConstant());
         assertEquals(-27, o2[0].getConstant());
+        assertEquals(0, o3[0].getConstant());
     }
 }
--- src/test/java/us/pinglesaur/algebraengine/functions/SqrtTest.java
+++ src/test/java/us/pinglesaur/algebraengine/functions/SqrtTest.java
@@ -21,18 +21,24 @@ public class SqrtTest {
         v1[0] = new Value(9);
         Value[] v2 = new Value[1];
         v2[0] = new Value(-9);
+        Value[] v3 = new Value[1];
+        v3[0] = new Value(0);
 
         Sqrt f1 = new Sqrt();
         Value[] o1 = f1.forwardExecute(v1, -100, 100);
         Value[] o2 = f1.forwardExecute(v2, -100, 100);
+        Value[] o3 = f1.forwardExecute(v3, -100, 100);
 
-        assert(o1 != null && o2 != null);
+        assert(o1 != null && o2 != null && o3 != null);
         assert(o1.length == 1);
         assert(o2.length == 1);
+        assert(o3.length == 1);
         assert(!o1[0].isImaginary);
         assert(o2[0].isImaginary);
+        assert(!o3[0].isImaginary);
         assertEquals(3, o1[0].getConstant());
         assertEquals(3, o2[0].getConstant());
+        assertEquals(0, o3[0].getConstant());
     }
 
     @Test
@@ -44,16 +50,24 @@ public class SqrtTest {
         Value[] v2 = new Value[1];
         v2[0] = new Value(3);
         v2[0].isImaginary = true;
+        Value[] v3 = new Value[1];
+        v3[0] = new Value(0);
+        v3[0].isImaginary = false;
 
         Sqrt f1 = new Sqrt();
         Value[] o1 = f1.reverseExecute(v1, -100, 100);
         Value[] o2 = f1.reverseExecute(v2, -100, 100);
+        Value[] o3 = f1.reverseExecute(v3, -100, 100);
 
-        assert(o1 != null && o2 != null);
+        assert(o1 != null && o2 != null && o3 != null);
         assert(o1.length == 1);
         assert(o2.length == 1);
-
+        assert(o3.length == 1);
+        assert(!o1[0].isImaginary);
+        assert(!o2[0].isImaginary);
+        assert(!o3[0].isImaginary);
         assertEquals(9, o1[0].getConstant());
         assertEquals(-9, o2[0].getConstant());
+        assertEquals(0, o3[0].getConstant());
     }
 }
