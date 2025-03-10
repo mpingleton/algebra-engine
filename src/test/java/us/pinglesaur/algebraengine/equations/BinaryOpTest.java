bf09e76da22fc247c046263d7b058072d9992ab0
Michael Pingleton <mnpingleton@gmail.com>
2025-03-10 08:54:04 -0400

    Added tests for BinaryOp swap.

--- src/test/java/us/pinglesaur/algebraengine/equations/BinaryOpTest.java
+++ src/test/java/us/pinglesaur/algebraengine/equations/BinaryOpTest.java
@@ -17,4 +17,16 @@ public class BinaryOpTest {
         assertEquals(3, BinaryOp.operatorPrecedence(BinaryOp.MOD));
         assertEquals(4, BinaryOp.operatorPrecedence(BinaryOp.POW));
     }
+
+    @Test
+    void testSwap() {
+        BinaryOp opA = new BinaryOp(new Value(3, "x"), BinaryOp.ADD, new Value(4, "x"));
+        BinaryOp opB = new BinaryOp(new Value(3.141), BinaryOp.POW, new Value(2));
+
+        opA.swap(opB);
+        assertEquals(Value.TYPE_CONSTANT, opA.l.type);
+        assertEquals(Value.TYPE_CONSTANT, opA.r.type);
+        assertEquals(Value.TYPE_VARIABLE, opB.l.type);
+        assertEquals(Value.TYPE_VARIABLE, opB.r.type);
+    }
 }
